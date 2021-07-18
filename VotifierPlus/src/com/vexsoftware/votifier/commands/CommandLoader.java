1ffb523cedad25d2714f985df15450aee4f17b85
BenCodez <benbergen12@gmail.com>
2021-07-18 14:17:13 -0400

    Add help command

--- VotifierPlus/src/com/vexsoftware/votifier/commands/CommandLoader.java
+++ VotifierPlus/src/com/vexsoftware/votifier/commands/CommandLoader.java
@@ -5,11 +5,17 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 import org.bukkit.command.CommandSender;
 
 import com.bencodez.advancedcore.api.command.CommandHandler;
 import com.vexsoftware.votifier.VotifierPlus;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -24,7 +30,37 @@ public class CommandLoader {
 		return instance;
 	}
 
+	public ArrayList<TextComponent> helpText(CommandSender sender) {
+		ArrayList<TextComponent> msg = new ArrayList<TextComponent>();
+		HashMap<String, TextComponent> unsorted = new HashMap<String, TextComponent>();
+
+		boolean requirePerms = false;
+		ChatColor hoverColor = ChatColor.AQUA;
+		for (CommandHandler cmdHandle : plugin.getCommands()) {
+			if (!requirePerms || cmdHandle.hasPerm(sender)) {
+				unsorted.put(cmdHandle.getHelpLineCommand("/votifierhelp"),
+						cmdHandle.getHelpLine("/votifierplus", "&6%Command% - &6%HelpMessage%", hoverColor));
+			}
+		}
+		ArrayList<String> unsortedList = new ArrayList<String>();
+		unsortedList.addAll(unsorted.keySet());
+		Collections.sort(unsortedList, String.CASE_INSENSITIVE_ORDER);
+		for (String cmd : unsortedList) {
+			msg.add(unsorted.get(cmd));
+		}
+
+		return msg;
+	}
+
 	public void loadCommands() {
+		plugin.getCommands()
+				.add(new CommandHandler(new String[] { "Help" }, "VotifierPlus.Help", "Open help page") {
+
+					@Override
+					public void execute(CommandSender sender, String[] args) {
+						sendMessageJson(sender, helpText(sender));
+					}
+				});
 		plugin.getCommands()
 				.add(new CommandHandler(new String[] { "Reload" }, "VotifierPlus.Reload", "Reload the plugin") {
 
